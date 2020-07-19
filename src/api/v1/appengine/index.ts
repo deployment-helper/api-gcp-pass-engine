d1936f735b5b19e46b5e6c372d79347d3e22bc0e
Vinay Mavi <vinaymavi@gmail.com>
2020-07-19 21:29:29 +0530

    Get endpoint to post

--- src/api/v1/appengine/index.ts
+++ src/api/v1/appengine/index.ts
@@ -4,6 +4,6 @@ const ctrl = new AppEngineController();
 const router = Router();
 
 router.post("/", ctrl.create.bind(ctrl));
-router.get("/", ctrl.get.bind(ctrl));
+router.post("/getapp", ctrl.get.bind(ctrl));
 
 export { router as appengineRouter };
